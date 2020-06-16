d05aa33edc6c0dcea39baf2d0170fe76246426fb
lucasfrutig0 <lucasfrutig0@gmail.com>
2020-06-16 16:00:28 -0300

    feat: add Yup (Data Validation)

--- backend/src/app/controllers/StudentsController.js
+++ backend/src/app/controllers/StudentsController.js
@@ -1,7 +1,20 @@
+import * as Yup from 'yup';
 import Students from '../models/Students';
 
 class StudentsController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().email().required(),
+      age: Yup.number().required(),
+      weight: Yup.number().required(),
+      height: Yup.number().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation Fails' });
+    }
+
     const studentExists = await Students.findOne({
       where: { email: req.body.email },
     });
@@ -16,6 +29,19 @@ class StudentsController {
   }
 
   async update(req, res) {
+    const schema = Yup.object().shape({
+      id: Yup.number().required(),
+      name: Yup.string(),
+      email: Yup.string().email(),
+      age: Yup.number(),
+      weight: Yup.number(),
+      height: Yup.number(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation Fails' });
+    }
+
     const student = await Students.findByPk(req.body.id);
 
     if (!student) {
